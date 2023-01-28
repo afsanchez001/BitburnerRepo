c1c7cf6970f6fa8d36f8037782ed8a32fef0cd3c
Anthony F Sanchez <afsanchez001@hotmail.com>
2023-01-28 14:30:20 -0800

    Update opti-launcher.js

--- OptiRunner/opti-launcher.js
+++ OptiRunner/opti-launcher.js
@@ -3,10 +3,10 @@ export async function main(ns) {
 
     ns.disableLog('ALL');
 
-	var servers = ["pserv-0", "pserv-1", "pserv-2", "pserv-3", "pserv-4", "pserv-5", "pserv-6", "pserv-7", "pserv-8", "pserv-9",
-		"pserv-10", "pserv-11", "pserv-12", "pserv-13", "pserv-14", "pserv-15", "pserv-16", "pserv-17", "pserv-18", "pserv-19",
-		"pserv-20", "pserv-21", "pserv-22", "pserv-23", "pserv-24"
-	];
+    var servers = ["pserv-0", "pserv-1", "pserv-2", "pserv-3", "pserv-4", "pserv-5", "pserv-6", "pserv-7", "pserv-8", "pserv-9",
+        "pserv-10", "pserv-11", "pserv-12", "pserv-13", "pserv-14", "pserv-15", "pserv-16", "pserv-17", "pserv-18", "pserv-19",
+        "pserv-20", "pserv-21", "pserv-22", "pserv-23", "pserv-24"
+    ];
 
     // loop through purchased servers
     for (var i = 0; i < servers.length; i++) {
