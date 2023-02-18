29e9aa84b47c82ada264926dec4cd12039766290
Anthony F Sanchez <afsanchez001@hotmail.com>
2023-02-18 12:27:03 -0800

    Update bigtrouble.js

--- big-trouble-achievement/bigtrouble.js
+++ big-trouble-achievement/bigtrouble.js
@@ -1,46 +1,45 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-	//ns.disableLog("getServerMaxMoney");
-	//ns.disableLog("getServerMoneyAvailable");
+    ns.disableLog("getServerMaxRam");
+    ns.disableLog("getServerUsedRam");
 
-	ns.disableLog("getServerMaxRam");
-	ns.disableLog("getServerUsedRam");
+    ns.disableLog("sleep");
 
-	ns.disableLog("sleep");
+    ns.clearLog();
+    ns.tail();
 
-	ns.clearLog();
-	ns.tail();
+    var spammy_line = "------------------------------------------"; // don't complain.
+    var target = "n00dles"; // Change the target here.
 
-	var spammy_line = "------------------------------------------"; 
-	var target = "sigma-cosmetics"; // Change the target here.
+    while (true) {
 
-	while (true) {
-	
-		let time = new Date().toLocaleTimeString();
+        if (ns.getServerMoneyAvailable(target) == 0) {
+            ns.print("'Big Trouble' achieved! $0.00 remain on " + target);
+            return;
+        }
 
-		ns.print(spammy_line); // don't complain.
+        let time = new Date().toLocaleTimeString();
 
-		ns.getServerMaxMoney(target);
-		ns.getServerMoneyAvailable(target);
+        ns.print(spammy_line); 
 
-		let available_threads_hk = 0;
-		let available_threads_wk = 0;
+        ns.getServerMaxMoney(target); // We show this in the tail window
+        ns.getServerMoneyAvailable(target); // We show this in the tail window
 
-		available_threads_hk = threadCount(ns, target, 1.70); // bigtrouble-hack.js SCRIPT RAM, 1.70GB
-		ns.exec("bigtrouble-hack.js", "home", available_threads_hk, target, false, time);
+        let available_threads_hk = threadCount(ns, target, 1.70); // bigtrouble-hack.js SCRIPT RAM, 1.70GB
+        ns.exec("bigtrouble-hack.js", "home", available_threads_hk, target, false, time);
 
-		available_threads_wk = threadCount(ns, target, 1.75); // bigtrouble-weaken.js SCRIPT RAM, 1.75GB
-		ns.exec("bigtrouble-weaken.js", "home", available_threads_wk, target, false, time);
+        let available_threads_wk = threadCount(ns, target, 1.75); // bigtrouble-weaken.js SCRIPT RAM, 1.75GB
+        ns.exec("bigtrouble-weaken.js", "home", available_threads_wk, target, false, time);
 
-		await ns.sleep(100);
-	}
+        await ns.sleep(100);
+    }
 }
 
 // Convert hostname & scriptRam into a number of threads that represents the server's total capacity.
 function threadCount(ns, hostname, scriptRam) {
-	let threads = 0;
-	let free_ram = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname);
-	threads = free_ram / scriptRam;
-	return Math.floor(threads) // Flooring this returns an integer. Avoids returning half a thread, or 1.5 threads, etc. 
+    let threads = 0;
+    let free_ram = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname);
+    threads = free_ram / scriptRam;
+    return Math.floor(threads) // Flooring this returns an integer. Avoids returning half a thread, or 1.5 threads, etc. 
 }
