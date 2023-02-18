351f065433b72635d350f49db677dc4fc71fa28a
Anthony F Sanchez <afsanchez001@hotmail.com>
2023-02-18 12:16:06 -0800

    Create bigtrouble-weaken.js

---
+++ big-trouble-achievement/bigtrouble-weaken.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	let target = ns.args[0];
+	let repeat = ns.args[1];
+	do {
+		try {
+			await ns.weaken(target);
+		} catch {
+			//
+		}
+	} while (repeat)
+}
