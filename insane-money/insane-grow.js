16f3e4e26518744416f6b026d6970da2c0ddff18
Anthony F Sanchez <afsanchez001@hotmail.com>
2023-02-19 11:29:23 -0800

    Create insane-grow.js

---
+++ insane-money/insane-grow.js
@@ -0,0 +1,7 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    var server = ns.args[0];
+    var sleeptime = ns.args[1];
+    await ns.sleep(sleeptime);
+    await ns.grow(server);
+}
